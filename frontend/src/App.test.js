1e568c132af6c83b706f313370243808fb47d304
Austinjnr <Austinobonyo@gmail.com>
2023-04-17 10:48:21 +0300

    fixes bug in workflow

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+const { add, subtract } =  require('./cashflow');
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('cashflow.js tests', () =>{
+    it ('should test add', () =>{
+        expect(add(2, 2)).toBe(4);
+    })
+    it ('should test subtract', () =>{
+        expect(subtract(2, 2)).toBe(0);
+    })
+})
